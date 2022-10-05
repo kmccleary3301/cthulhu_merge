6a72f5b7203f9d1654f2c5008552fb0d666923fc
kmccleary3301 <kmccl24@lsu.edu>
2022-10-05 16:45:48 -0500

    modified:   create_octo.sh

--- create_octo.sh
+++ create_octo.sh
@@ -18,9 +18,9 @@ do
     git checkout -b octo_branch_$i;
     for j in {1..3}
     do
-        sed -i -e "s/line $i _/line $i _ $RANDOM/g" octo_test.txt;
+        sed -n -i -e "s/line $i _/line $i _ $RANDOM/g" octo_test.txt;
         git add octo_test.txt;
-        git commit -m "Commit $j for branch $i";
+        git commit -m "Commit $j for branch $i" &> /dev/null;
     done
     # Since we want all 8 branches based off same branch, checkout master.
     git checkout master;
