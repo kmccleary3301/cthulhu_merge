bfc36871d6de291a0477cef1f151cd397a8905b4
kmccleary3301 <kmccl24@lsu.edu>
2022-10-05 15:34:01 -0500

    modified:   create_octo.sh

--- create_octo.sh
+++ create_octo.sh
@@ -5,7 +5,7 @@
 # that branch changes 1 line, and commits - 3x in a row
 # then do the same with a different line, on a different branch
 
-make_octo_txt.sh $1
+./make_octo_txt.sh $1
 
 # Now add and commit this file
 git add octo_test.txt
@@ -16,7 +16,7 @@ git commit -m "Added the octo file with $1 lines"
 for i in $(eval echo "{1..$1}")
 do
     git checkout -b octo_branch_$i;
-    for j in {1..3}
+    for j in {1..5}
     do
         sed -i -e "s/line $i _/line $i _ $RANDOM/g" octo_test.txt;
         git add octo_test.txt;
